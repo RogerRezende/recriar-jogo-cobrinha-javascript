c305941a0e27e15edf7d95a1eacf1de120bcc2a5
Roger Rezende <roger.rezendegmail.com>
2020-09-03 15:34:54 -0300

    criação da função criar cobrinha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+//variáveis
+let canvas = document.getElementById("snake"); 
+let context = canvas.getContext("2d"); 
 let box = 32;
-
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+let snake = []; 
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
-
+//função para criar o background do jogo
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box);
+}
+//função para criar a cobrinha
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+//chamadas das funções
 criarBG();
+criarCobrinha();
